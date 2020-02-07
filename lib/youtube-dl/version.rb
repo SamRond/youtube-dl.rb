c9ee43a471482bf574faa7af35724a284ea345dc
Sam Rondinelli <sam.rondinelli3@gmail.com>
2020-02-07 15:54:46 -0700

    Updated binaries to 2020.01.24

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.2.2019.07.14'.freeze
+  VERSION = '0.3.2.2020.01.24'.freeze
 end
